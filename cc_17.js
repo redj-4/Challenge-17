bdbf40ee6196d5c6468e134d67b3c96050e5f727
Divya Joshi <divyaj.redj@gmail.com>
2025-03-30 22:31:12 -0400

    Task 2 - Sales rep’s clients and total spent for a specific client.

--- cc_17.js
+++ cc_17.js
@@ -22,3 +22,28 @@ customer1.addPurchase(85.75);
 customer1.addPurchase(40.00);
 
 console.log(`Total spent by ${customer1.name}: $${customer1.getTotalSpent()}`); //logging in the console waht the custome name and total spent by the customer
+
+//Task 2: Create a salesRep class 
+class SalesRep { //creating a class for salesRep
+    constructor(name) { 
+        this.name = name; 
+        this.clients = []; 
+    }
+
+    addClient(customer) {
+        this.clients.push(customer); //Adding a customer to the clients array 
+    }
+    getClientTotal(name) {
+        const client = this.clients.find(c => c.name === name);
+        return client ? client.getTotalSpent() : 0;
+    } //Find client by name
+}
+
+//logging the salesRep clients 
+const customer2 = new Customer('Bryce Waters', 'BryceW@gmail.com');
+customer2.addPurchase(120.50);
+customer2.addPurchase(85.75);
+
+const salesRep1 = new SalesRep('Carl Ramirez');
+salesRep1.addClient(customer2);
+salesRep1.getClientTotal('Bryce Waters');
